853caf0c2462760805564e930e9cdbd98c4e4c5f
dalv95 <dalv95@gmail.com>
2020-05-03 14:05:18 +0200

    styles for visits

--- js/index.js
+++ js/index.js
@@ -12,15 +12,9 @@ $(document).ready(function () {
     $("#book_toggle").click(function () {
         $("#book-form_col-2").slideToggle()
         $("#map").slideToggle()
-        $("#visits").slideToggle()
     });
 });
 
-$(document).ready(function () {
-    $("#result_toggle").click(function () {
-        $("#result-form_col-2").slideToggle()
-    });
-});
 
 $(document).ready(function () {
     $("#result_toggle").click(function () {
